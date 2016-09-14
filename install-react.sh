17b15ea77f0592fda0404addc68f26074ff5b0f8
Artem Demo <artemdemo@gmail.com>
2016-09-14 12:11:40 +0300

    fixed quotes

--- install-react.sh
+++ install-react.sh
@@ -17,38 +17,38 @@ mkdir source/components
 mkdir source/constants
 mkdir source/reducers
 
-printf "--> Installing babel... \n"
+printf '--> Installing babel... \n'
 npm i --save babel babel-plugin-transform-object-assign babel-plugin-transform-react-jsx babel-preset-es2015
 
-printf "--> Installing less... \n"
+printf '--> Installing less... \n'
 npm i --save less
 
-printf "--> Installing react... \n"
+printf '--> Installing react... \n'
 npm i --save react react-dom react-router history
 
-printf "--> Installing redux... \n"
+printf '--> Installing redux... \n'
 npm i --save react-redux redux react-router-redux redux-thunk
 
-printf "--> Installing webpack... \n"
+printf '--> Installing webpack... \n'
 npm i --save webpack webpack-dev-server
 
-printf "--> Installing loaders... \n"
+printf '--> Installing loaders... \n'
 npm i --save babel-loader css-loader style-loader less-loader postcss-loader url-loader file-loader html-loader
 
-printf "--> Installing plugins... \n"
+printf '--> Installing plugins... \n'
 npm i --save classnames html-webpack-plugin extract-text-webpack-plugin autoprefixer
 
-printf "--> Installing eslint... \n"
+printf '--> Installing eslint... \n'
 npm i --save eslint git+https://github.com/artemdemo/eslint-config-artem-react.git#1.0.0
 
-printf "--> Installing bootstrap css... \n"
+printf '--> Installing bootstrap css... \n'
 npm i --save bootstrap
 
 printf "${YELLOW} Done${NC} You can add scripts to package.json: \n"
-printf "{ \n"
-printf "  \"start\": \"npm run server\", \n"
-printf "  \"server\": \"webpack-dev-server\", \n"
-printf "  \"build\": \"webpack --progress --colors\", \n"
-printf "  \"watch\": \"webpack --progress --colors --watch\", \n"
-printf "... \n"
-printf "} \n"
+printf '{ \n'
+printf '  "start": "npm run server", \n'
+printf '  \"server\": \"webpack-dev-server\", \n'
+printf '  \"build\": \"webpack --progress --colors\", \n'
+printf '  \"watch\": \"webpack --progress --colors --watch\", \n'
+printf '... \n'
+printf '} \n'
