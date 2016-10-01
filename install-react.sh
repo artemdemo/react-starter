f13c3e656f88d2267b580fa434b87632b1aed3f2
Artem Demo <artemdemo@gmail.com>
2016-10-01 12:12:59 +0300

    installing packages script

--- install-react.sh
+++ install-react.sh
@@ -18,7 +18,8 @@ mkdir source/constants
 mkdir source/reducers
 
 printf ' --> Installing babel... \n'
-npm i --save babel babel-plugin-transform-object-assign babel-plugin-transform-react-jsx babel-preset-es2015
+npm i --save babel babel-core babel-preset-es2015
+npm i --save babel-plugin-transform-object-assign babel-plugin-transform-react-jsx
 
 printf ' --> Installing less... \n'
 npm i --save less
@@ -27,19 +28,22 @@ printf ' --> Installing react... \n'
 npm i --save react react-dom react-router history
 
 printf ' --> Installing redux... \n'
-npm i --save react-redux redux react-router-redux redux-thunk
+npm i --save redux react-redux react-router-redux
+npm i --save redux-thunk
 
 printf ' --> Installing webpack... \n'
 npm i --save webpack webpack-dev-server
 
 printf ' --> Installing loaders... \n'
-npm i --save babel-loader css-loader style-loader less-loader postcss-loader url-loader file-loader html-loader
+npm i --save babel-loader css-loader style-loader
+npm i --save less-loader postcss-loader url-loader file-loader html-loader
 
 printf ' --> Installing plugins... \n'
-npm i --save classnames html-webpack-plugin extract-text-webpack-plugin autoprefixer
+npm i --save classnames autoprefixer
+npm i --save html-webpack-plugin extract-text-webpack-plugin
 
 printf ' --> Installing eslint... \n'
-npm i --save eslint git+https://github.com/artemdemo/eslint-config-artem-react.git#1.0.0
+npm i --save eslint git+https://github.com/artemdemo/eslint-config-artem-react.git#1.1.0
 
 printf ' --> Installing bootstrap css... \n'
 npm i --save bootstrap
