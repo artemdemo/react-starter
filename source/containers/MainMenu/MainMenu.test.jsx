43c20b6955712f4ad28473fb060eaecb7c5ed080
Artem Demo <artemdemo@gmail.com>
2017-03-18 16:38:52 +0200

    MainMenu tests

---
+++ source/containers/MainMenu/MainMenu.test.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import MainMenu from './MainMenu';
+
+describe('<MainMenu>', () => {
+    it('Render', () => {
+        const tree = renderer.create(
+            <MainMenu/>
+        ).toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
+});
