a1e4bc3ce106a21ef39aa03286a7c3128eb79b42
Artem Demo <artemdemo@gmail.com>
2018-05-27 22:12:46 +0300

    added explanation for webpack 4 change

--- source/store.js
+++ source/store.js
@@ -5,9 +5,10 @@ import _isFunction from 'lodash/isFunction';
 import combinedReducers from './reducers';
 import rootSaga from './sagas';
 
-// There is some weird bug with combination fo sagas and webpack 4
-// So it's not getting `default` export even if I'm asking it to
-// The work around is to do it explicitly
+// There is breaking change in webpack 4
+// It's related to CommonJs modules (which is `redux-saga`)
+// Fot this modules webpack is adding additional `default`
+// @link https://medium.com/webpack/webpack-4-import-and-commonjs-d619d626b655
 const sagaMiddleware = _isFunction(createSagaMiddleware) ? createSagaMiddleware() : createSagaMiddleware.default();
 
 const store = createStore(combinedReducers, applyMiddleware(sagaMiddleware));
