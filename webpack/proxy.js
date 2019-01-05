524c6f195703c1730524b9ca0640cbaac6e149bd
Artem Demo <artemdemo@gmail.com>
2019-01-05 15:10:47 +0200

    added "/email" endpoint

--- webpack/proxy.js
+++ webpack/proxy.js
@@ -1,7 +1,10 @@
 /* eslint-disable consistent-return, max-len,import/newline-after-import */
 
-const campaigns = require('./mock-data/campaigns.json');
 const campaignsRegex = /\/api\/campaigns/;
+const campaigns = require('./mock-data/campaigns.json');
+
+const emailsRegex = /\/api\/email/;
+const emails = require('./mock-data/emails.json');
 
 module.exports = {
     '/api': {
@@ -9,6 +12,9 @@ module.exports = {
             const testUrl = (urlRegex, method = 'GET') => urlRegex.test(req.url) && req.method === method;
 
             switch (true) {
+                case testUrl(emailsRegex):
+                    res.json(emails);
+                    return true;
                 case testUrl(campaignsRegex):
                     res.json(campaigns);
                     return true;
