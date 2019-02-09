fe3127c08c135f54ba95ac4d80e6435372af46f4
Artem Demo <artemdemo@gmail.com>
2019-02-09 21:26:35 +0200

    added proposal class properties

--- babel.config.js
+++ babel.config.js
@@ -10,6 +10,7 @@ module.exports = {
     plugins: [
         '@babel/plugin-transform-react-jsx',
         '@babel/plugin-transform-object-assign',
+        '@babel/plugin-proposal-class-properties',
         '@babel/plugin-proposal-object-rest-spread',
         '@babel/plugin-syntax-dynamic-import',
         ['@babel/plugin-transform-runtime', {
