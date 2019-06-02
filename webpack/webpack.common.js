46f8104e35d81fed395e6d1d257d4bb7855ca2ef
Artem Demo <artemdemo@gmail.com>
2019-06-02 07:46:59 +0300

    updated clean-webpack-plugin

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,5 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const {
     IgnorePlugin,
     DefinePlugin,
