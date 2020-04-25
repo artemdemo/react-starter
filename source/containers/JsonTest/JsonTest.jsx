5e7ff6acb5b8441a3658022f4d2928d97a36f257
Artem Demo <artemdemo@gmail.com>
2020-04-25 15:06:50 +0300

    updated JsonTest

--- source/containers/JsonTest/JsonTest.jsx
+++ source/containers/JsonTest/JsonTest.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
 import data from './data.json';
+
+/*
+ * There have been problems with importing json for certain versions of webpack.
+ * Therefore I'm testing it to be sure that I can update to the latest version.
+ */
+
+const JsonContent = styled.pre`
+    font-size: 0.8em;
+    background: #f4f6f7;
+    border: 1px solid #ECEFF1;
+    padding: 5px 8px;
+    border-radius: 3px;
+    display: inline-block;
+`;
 
 const JsonTest = () => {
     return (
@@ -7,9 +22,9 @@ const JsonTest = () => {
             <p>
                 JSON import example:
             </p>
-            <pre>
+            <JsonContent>
                 {JSON.stringify(data, null, 2)}
-            </pre>
+            </JsonContent>
         </React.Fragment>
     );
 };
