adaaee3885221a13dae72933ad480b33196cabe2
Artem Demo <artemdemo@gmail.com>
2021-11-27 14:57:14 +0200

    removed unused mocks

--- __mocks__/@fortawesome/react-fontawesome.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import _omit from 'lodash/omit';
-
-export const FontAwesomeIcon = (props) => (
-  <div data-mock="FontAwesomeIcon">
-    {JSON.stringify(_omit(props, ['children']), null, 2)}
-    {props.children}
-  </div>
-);
