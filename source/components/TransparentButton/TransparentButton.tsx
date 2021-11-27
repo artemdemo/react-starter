810b3afd3a26353d84394b3d543ac4fede1de559
Artem Demo <artemdemo@gmail.com>
2021-11-27 13:39:42 +0200

    use modules css

--- source/components/TransparentButton/TransparentButton.tsx
+++ source/components/TransparentButton/TransparentButton.tsx
@@ -1,11 +1,13 @@
 import React, {ButtonHTMLAttributes} from 'react';
 import classnames from 'classnames';
-import './TransparentButton.css';
+import s from './TransparentButton.module.css';
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {}
 
-export const TransparentButton: React.FC<Props> = (props) => (
-  <button className={classnames('TransparentButton', props.className)} onClick={props.onClick}>
-    {props.children}
-  </button>
-);
+export const TransparentButton: React.FC<Props> = (props) => {
+  return (
+    <button className={classnames(s.TransparentButton, props.className)} onClick={props.onClick}>
+      {props.children}
+    </button>
+  )
+};
