4f13d8ab8142c00c49ac318f3b82306797fa24af
Artem Demo <artemdemo@gmail.com>
2022-09-29 22:45:28 +0300

    removed sagas and redux

--- packages/app/src/testApp/TestApp.tsx
+++ packages/app/src/testApp/TestApp.tsx
@@ -1,28 +1,24 @@
 import React from 'react';
+import axios from 'axios';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import { Routing } from './Routing';
 import { Header } from './Header';
 import { AppProvider } from '../contexts/app/AppContext';
 import Container from '../components/Container/Container';
-import store from './store';
 import { HttpProvider } from '../contexts/http/HttpContext';
-import axios from 'axios';
 
 export const TestApp: React.FC = () => {
   return (
-    <Provider store={store}>
-      <Router>
-        <AppProvider appVersion={ENV.appVersion}>
-          <HttpProvider httpClient={axios}>
-            <Container>
-              <Header />
-              <hr />
-              <Routing />
-            </Container>
-          </HttpProvider>
-        </AppProvider>
-      </Router>
-    </Provider>
+    <Router>
+      <AppProvider appVersion={ENV.appVersion}>
+        <HttpProvider httpClient={axios}>
+          <Container>
+            <Header />
+            <hr />
+            <Routing />
+          </Container>
+        </HttpProvider>
+      </AppProvider>
+    </Router>
   );
 };
--- packages/app/src/testApp/sagas.ts
+++
@@ -1,6 +0,0 @@
-import { all } from 'redux-saga/effects';
-import emails from './model/emails/emailsSagas';
-
-export default function* rootSaga() {
-  yield all([emails()]);
-}
