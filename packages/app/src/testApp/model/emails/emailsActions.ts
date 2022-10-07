f6ba38dd0a24e54f1884ca11f23afe9f7bc7c60e
Artem Demo <artemdemo@gmail.com>
2022-10-07 11:23:33 +0300

    removed unused shit

--- packages/app/src/testApp/model/emails/emailsActions.ts
+++
@@ -1,5 +0,0 @@
-import { createAction } from 'redux-actions';
-
-export const loadEmails = createAction('LOAD_EMAILS');
-export const emailsLoaded = createAction('EMAILS_LOADED');
-export const emailsLoadingError = createAction('EMAILS_LOADING_ERROR');
