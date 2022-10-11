21823314e8f2ac831eb116d34d690616a080c7fe
Artem Demo <artemdemo@gmail.com>
2022-10-11 11:11:12 +0300

    moved commands around

--- packages/builder-webpack/src/builder-webpack.ts
+++ packages/builder-webpack/src/builder-webpack.ts
@@ -7,17 +7,17 @@ console.log('--><--');
 
 cliArgsParser([
   {
-    name: 'build',
-    description: 'Builds the project',
-    action: (str, options) => {
-      console.log('build action');
-    }
-  },
-  {
     name: 'start',
     description: 'Starts the project',
     action: (str, options) => {
       console.log('start action');
+    }
+  },
+  {
+    name: 'build',
+    description: 'Builds the project',
+    action: (str, options) => {
+      console.log('build action');
     }
   },
 ]);
